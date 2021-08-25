eede65ddaee7f480a3632f44733f5c84b2edd2a7
Kyle <tigerlove95@naver.com>
2021-08-25 16:13:54 +0900

    210825

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar'
+import {BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import About from './components/About';
+import Resume from './components/Resume';
+import Projects from './components/Projects';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h1>Uff! Finally</h1>
+      <div className="container">
+        <div className="row">
+      <div className="col-lg-3">
+       
+          <Sidebar />
+        
+      </div>
+      <div className='col-lg-9 app__main-content'>
+
+        <Navbar />
+        <Switch>
+    <Route exact path="/" component={About} />
+
+    <Route path="/resume" component={Resume} />
+
+    <Route path="/projects" component={Projects} />
+    <Route>
+      <Redirect to="/" />
+    </Route>
+    </Switch>
+      </div>      
     </div>
+    </div>
+    </div>
+    </Router>
   );
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import github from '../assets/icons/github.svg'
+import pin from '../assets/icons/pin.svg'
+import tie from '../assets/icons/tie.svg'
+import mightycoder from '../assets/mightycoder.svg'
+import resume from '../assets/resume.pdf'
+
+const Sidebar = () => {
+  const handleEmailme = () => {
+    window.open("mailto: tigerlove95@naver.com")
+  }
+  return (
+    <div className="sidebar">
+      <img src={mightycoder} alt="avatar" className="sidebar__avatar" />
+      <div className="sidebar__name">조 <span>강훈</span></div>
+      <div className="sidebar__item sidebar__title">Junior Front-End Developer</div>
+      <a href={resume} download="resume.pdf">
+        <div className="sidebar__item sidebar__resume">
+          <img src={tie} alt="resume" className="sidebar__icon" />이력서 다운로드
+        </div>
+      </a>
+      <figure className="sidebar__social-icons my-2">
+        <a href="">
+          <img src="" alt="upInAir2" className="sidebar__icon mr-3" />
+        </a>
+        <a href="">
+          <img src="" alt="upInAir2" className="sidebar__icon" />
+        </a>
+      </figure>
+      <div className="sidebar__contact">
+        <div className="sidebar__item sidebar__github">
+          <a href="#">
+            <img src={github} alt="github" className="sidebar__icon mr-3" />github
+          </a>
+          </div>
+          <div className="sidebar__location">
+            <img src={pin} alt="location" className="sidebar__icon mr-3" />
+            부천, 대한민국</div>
+          <div className="sidebar__item">tigerlove95@naver.com</div>
+          <div className="sidebar__item">010-6611-2574</div>
+      </div>
+      <div className="sidebar__item sidebar__email" onClick={handleEmailme}>Email me</div>
+
+    </div>
+  )
+}
+
+export default Sidebar
