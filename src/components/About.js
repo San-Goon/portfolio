ae705f7fb7c8b32843907e03a26895609bb74f3a
Kyle <tigerlove95@naver.com>
2021-08-26 20:55:05 +0900

    210826

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import api from "../assets/icons/api.svg";
+import Skillcard from "./Skillcard";
+
+const skills = [
+  {
+    icon: api,
+    title: "채워주세요",
+    about: "채워주세요",
+  },
+  {
+    icon: api,
+    title: "채워주세요",
+    about: "채워주세요",
+  },
+];
 
 const About = () => {
   return (
-  <div>
-    <h1>This is About</h1>
-  </div>
-  )
-}
+    <div className="about">
+      <h6 className="about__intro">소개란</h6>
+      <div className="container about__container">
+        <h6 className="about__heading">what I offer</h6>
+        <div className="row">
+          {skills.map((skill) => (
+            <Skillcard skill={skill} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
