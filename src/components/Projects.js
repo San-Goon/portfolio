b7811719c07528767c98b70ee1535d3a05535e5a
Kyle <tigerlove95@naver.com>
2021-09-03 12:08:42 +0900

    210903

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,16 @@
 import React from "react";
+import data_projects from "./data/projects_data";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
     <div>
       <div className="container projects">
-        <div className="row">{}</div>
+        <div className="row">
+          {data_projects.map((project) => (
+            <ProjectCard key={project.name} project={project} />
+          ))}
+        </div>
       </div>
     </div>
   );
